767d25e7e1a111133c7e1000570ae70b89b1cef7
mwojas <michal.wojas@sabre.com>
2020-03-27 14:24:12 +0000

    a.py it is ...

---
+++ a.py
@@ -0,0 +1,26 @@
+import firebase_admin
+from firebase_admin import credentials
+
+# cred = credentials.Cert('path/to/serviceKey.json')
+cred = firebase_admin.credentials.Certificate('path/to/serviceKey.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : 'https://qwiklabs-gcp-03-5addf9d90e77.firebaseio.com'
+})
+
+from firebase_admin import db
+
+root = db.reference()
+# Add a new user under /users.
+new_user = root.child('users').push({
+    'name' : 'Mary Anning', 
+    'since' : 1700
+})
+
+# Update a child attribute of the new user.
+new_user.update({'since' : 1799})
+
+# Obtain a new reference to the user, and retrieve child data.
+# Result will be made available as a Python dict.
+mary = db.reference('users/{0}'.format(new_user.key)).get()
+print('Name:', mary['name'])
+print('Since:', mary['since'])
